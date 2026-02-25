370a317523e9de8486e215bb31cd338daf0e2d63
GouravS27 <gauravsingh27012000@gmail.com>
2026-02-26 01:35:00 +0530

    Added Feed API and enable CORS for frontend

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ const connectionRequest = require("./src/routes/connectionRequestRouter")
 
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const userRouter = require("./src/routes/userRoutes");
 
 const app = express();
+
+app.use(cors({
+  origin:"http://localhost:5173/",
+  credentials:true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/userAuth");
-const connectionRequest = require("../models/connectionRequest");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const userRouter = express.Router();
 
@@ -10,7 +11,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
 
-    const data = await connectionRequest
+    const data = await ConnectionRequest
       .find({
         toUserId: loggedInUser,
         status: "interested",
@@ -23,32 +24,71 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   }
 });
 
-userRouter.get('/user/connections',userAuth,async (req,res)=>{
-    try{
-        const loggedInUser = req.user;
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
 
-        const connReq = await connectionRequest.find({
-            $or:[
-                {fromUserId:loggedInUser,status:"accepted"},
-                {toUserId:loggedInUser,status:"accepted"}
-            ]
-        }).populate("fromUserId",USER_SAFE_DATA)
-          .populate("toUserId",USER_SAFE_DATA);
+    const connReq = await ConnectionRequest
+      .find({
+        $or: [
+          { fromUserId: loggedInUser, status: "accepted" },
+          { toUserId: loggedInUser, status: "accepted" },
+        ],
+      })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
 
-        // console.log(connReq)
+    // console.log(connReq)
 
-        const data = connReq.map((row)=>{
-            if(row.fromUserId._id.toString() === loggedInUser._id.toString())
-                return row.toUserId;
-            return row.fromUserId;
-        });
+    const data = connReq.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString())
+        return row.toUserId;
+      return row.fromUserId;
+    });
 
-        res.json({data});
-    }
-    
-    catch(err){
-        res.send(err.message);
-    }
-})
+    res.json({ data });
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+
+    const skip = (page - 1) * limit;
+
+    const connReq = await ConnectionRequest
+      .find({
+        $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+      })
+      // .select("firstName lastName");
+
+    const hideUsersFromFeed = new Set();
+
+    connReq.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.json({ data: users });
+  } catch (err) {
+    res.send(err.message);
+  }
+});
 
 module.exports = userRouter;
